2e0ce4043c383e3514c5512437d321b5deb2429c
rapomerantz <34749153+rapomerantz@users.noreply.github.com>
2018-03-28 16:46:06 -0500

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,30 @@
+HELLO
+
+-- 1 Get all users from Chicago
+SELECT * FROM "accounts" WHERE "city" ILIKE 'Chicago';
+
+--2 Get all usernames that contain the letter a
+SELECT * FROM "accounts" WHERE "username" ILIKE '%a%';
+
+--3 Give new acct balance of 10.00
+UPDATE "accounts" SET "account_balance" = 10 WHERE "transactions_attempted" = 0 AND "account_balance" = 0;
+
+--4 Select all users that have attempted 9 or more transactions.
+SELECT * FROM "accounts" WHERE "transactions_attempted" > 9;
+
+--5 Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
+SELECT "username", "account_balance" FROM "accounts" ORDER BY "account_balance" DESC LIMIT 3;
+
+--6 Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
+SELECT "username", "account_balance" FROM "accounts" ORDER BY "account_balance" ASC LIMIT 3;
+
+--7 Get all users with account balances that are more than $100.
+SELECT "username", "account_balance" FROM "accounts" WHERE "account_balance" > 100;
+
+
+--8 Add a new record.
+INSERT INTO accounts (username, city, transactions_completed, transactions_attempted, account_balance) VALUES ('atticus', 'minneapolis', 15, 15, 1500.00);
+
+--9 Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+SELECT * FROM "accounts" WHERE ("city" = 'miami' OR "city" = 'phoenix') AND "transactions_completed" < 5; 
+
